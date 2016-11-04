bcb66c2650fedd8e64b87a38f0961dc519a72741
ArunSinghJ <accord.j7@gmail.com>
2016-11-04 14:33:56 -0400

    Update MainScript.sh

--- MainScript.sh
+++ MainScript.sh
@@ -5,7 +5,8 @@ yum install gcc -y
 git clone https://github.com/ArunSinghJ/RaspiLogging
 gcc /root/RaspiLogging/ExpHmax.c -o ExpHmax
 chmod +x ExpHmax Newlogscript.sh ExpHmax.c
-echo "@reboot ./root/RaspiLogging/Newlogscript.sh" > /etc/crontab
+sed -i -e 's/\r$//' Newlogscript.sh
+echo "@reboot ./RaspiLogging/Newlogscript.sh" > /etc/crontab
 reboot 
 
 
