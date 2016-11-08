5d176ad593cdccdc52e13d870f6f58582b1e3abc
ArunSinghJ <accord.j7@gmail.com>
2016-11-08 10:32:10 -0500

    Update MainScript.sh

--- MainScript.sh
+++ MainScript.sh
@@ -6,7 +6,7 @@ git clone https://github.com/ArunSinghJ/RaspiLogging
 gcc /root/RaspiLogging/ExpHmax.c -o ExpHmax
 chmod +x ExpHmax Newlogscript.sh ExpHmax.c
 sed -i -e 's/\r$//' Newlogscript.sh
-echo "@reboot ./RaspiLogging/Newlogscript.sh" > /etc/crontab
+echo "@reboot ./RaspiLogging/Newlogscript.sh" >> /etc/crontab
 reboot 
 
 
